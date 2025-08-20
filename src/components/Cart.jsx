fefe7721ca2a23775ecd5e58311dc06730f41887
“Vignesh <2300033852@kluniversity.in>
2025-08-20 10:03:00 +0530

    Initial commit for eCommerce frontend

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+
 import { useCart } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
-const BASE_URL = 'http://localhost:9090/back1';
+const BASE_URL = 'http://localhost:8989/back1';
 
 const Cart = () => {
   const { cartItems, removeFromCart } = useCart();
